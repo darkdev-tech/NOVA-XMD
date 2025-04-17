ed6fa8c525d430fa3b6bb853c9a23577adb14f81
Dark hacker 😈 <javieldev@gmail.com>
2025-04-17 10:39:46 +0300

    Update menu.js

--- novaplugins/menu.js
+++ novaplugins/menu.js
@@ -1,84 +1,89 @@
-const { prefix, botName } = require('./config'); // Ensure your config file has necessary values
+const { prefix, botName } = require('../config'); // Adjust path if needed
 
-// Menu Command Handler
-async function menuCommand(m) {
-  const command = m.text.trim().slice(prefix.length).split(' ')[0];
-
-  if (command === 'menu') {
-    const menu = `
-╭━━━━━━━【 ${botName} 】━━━━━━━╮
+module.exports = {
+  name: 'menu',
+  alias: ['help'],
+  category: 'general',
+  desc: 'Show full command list',
+  usage: `${prefix}menu`,
+  async exec(m, sock) {
+    const menuText = `
+╭━━━〔 ${botName || 'NOVA XMD BOT'} MENU 〕━━━╮
 ┃
-┃ 👑 *Owner Commands*:
-┃ ➸ ${prefix}owner - View owner info
-┃ ➸ ${prefix}shutdown - Shutdown the bot
-┃ ➸ ${prefix}restart - Restart the bot
-┃ ➸ ${prefix}setprefix <newPrefix> - Change command prefix
-┃ ➸ ${prefix}setlang <language> - Change the bot's language
-┃ ➸ ${prefix}logs - View bot logs
-┃ ➸ ${prefix}uptime - View bot uptime
-┃ ➸ ${prefix}block <user> - Block a user
-┃ ➸ ${prefix}unblock <user> - Unblock a user
+┃ 👑 *OWNER COMMANDS*
+┃ ┣ ${prefix}owner
+┃ ┣ ${prefix}shutdown
+┃ ┣ ${prefix}restart
+┃ ┣ ${prefix}setprefix <symbol>
+┃ ┣ ${prefix}block <user>
+┃ ┣ ${prefix}unblock <user>
+┃ ┣ ${prefix}uptime
+┃ ┣ ${prefix}broadcast <text>
+┃ ┣ ${prefix}mode [public/private/self]
+┃ ┣ ${prefix}setpp
+┃ ┣ ${prefix}join <group link>
+┃ ┗ ${prefix}leavegc
 ┃
-┃ 🛠️ *Admin Commands*:
-┃ ➸ ${prefix}kick <user> - Kick a user from the group
-┃ ➸ ${prefix}ban <user> - Ban a user from the group
-┃ ➸ ${prefix}mute <user> - Mute a user
-┃ ➸ ${prefix}unmute <user> - Unmute a user
-┃ ➸ ${prefix}setwelcome <message> - Set a custom welcome message
-┃ ➸ ${prefix}setgoodbye <message> - Set a custom goodbye message
-┃ ➸ ${prefix}addadmin <user> - Add a user as admin
-┃ ➸ ${prefix}removeadmin <user> - Remove a user as admin
-┃ ➸ ${prefix}clearall - Clear all members in the group
+┃ 🛡️ *GROUP COMMANDS*
+┃ ┣ ${prefix}kick @user
+┃ ┣ ${prefix}add <number>
+┃ ┣ ${prefix}promote @user
+┃ ┣ ${prefix}demote @user
+┃ ┣ ${prefix}mute
+┃ ┣ ${prefix}unmute
+┃ ┣ ${prefix}group [open/close]
+┃ ┣ ${prefix}tagall
+┃ ┣ ${prefix}setwelcome <msg>
+┃ ┣ ${prefix}setgoodbye <msg>
+┃ ┣ ${prefix}delwelcome
+┃ ┣ ${prefix}delgoodbye
+┃ ┗ ${prefix}antilink [on/off]
 ┃
-┃ 🎮 *Fun Commands*:
-┃ ➸ ${prefix}joke - Get a random joke
-┃ ➸ ${prefix}meme - Get a random meme
-┃ ➸ ${prefix}quote - Get a random motivational quote
-┃ ➸ ${prefix}flip - Flip a coin
-┃ ➸ ${prefix}roll - Roll a dice
-┃ ➸ ${prefix}8ball - Ask the Magic 8-Ball
-┃ ➸ ${prefix}riddle - Solve a riddle
-┃ ➸ ${prefix}love <name1> <name2> - Calculate love percentage
-┃ ➸ ${prefix}cat - Get a random cat image
-┃ ➸ ${prefix}dog - Get a random dog image
-┃ ➸ ${prefix}facts - Get a random fun fact
-┃ ➸ ${prefix}weather <city> - Get the weather forecast
+┃ ⚙️ *SETTINGS*
+┃ ┣ ${prefix}prefix <symbol>
+┃ ┣ ${prefix}welcome [on/off]
+┃ ┣ ${prefix}goodbye [on/off]
+┃ ┣ ${prefix}xp [on/off]
+┃ ┣ ${prefix}cooldown <sec>
+┃ ┣ ${prefix}lang <code>
+┃ ┣ ${prefix}autorespond [on/off]
+┃ ┣ ${prefix}log [on/off]
+┃ ┗ ${prefix}resetsettings
 ┃
-┃ 🔧 *Settings*:
-┃ ➸ ${prefix}prefix <newPrefix> - Change the command prefix
-┃ ➸ ${prefix}welcome <enable/disable> - Enable or disable welcome messages
-┃ ➸ ${prefix}goodbye <enable/disable> - Enable or disable goodbye messages
-┃ ➸ ${prefix}cooldown <seconds> - Set cooldown time between commands
-┃ ➸ ${prefix}xp <enable/disable> - Enable or disable XP system
-┃ ➸ ${prefix}lang <language> - Change the bot's language
-┃ ➸ ${prefix}timezone <timezone> - Set the bot's timezone
-┃ ➸ ${prefix}autorespond <enable/disable> - Enable or disable auto-response feature
-┃ ➸ ${prefix}log <enable/disable> - Enable or disable logging
-┃ ➸ ${prefix}resetsettings - Reset settings to default
+┃ 🔍 *SEARCH*
+┃ ┣ ${prefix}google <query>
+┃ ┣ ${prefix}image <query>
+┃ ┣ ${prefix}yt <query>
+┃ ┣ ${prefix}lyrics <song>
+┃ ┣ ${prefix}weather <city>
+┃ ┣ ${prefix}define <word>
+┃ ┣ ${prefix}news <topic>
+┃ ┗ ${prefix}translate <text>
 ┃
-┃ 📚 *Search Commands*:
-┃ ➸ ${prefix}search <query> - Search on Google
-┃ ➸ ${prefix}image <query> - Search for images on Google
-┃ ➸ ${prefix}video <query> - Search for videos on YouTube
-┃ ➸ ${prefix}news <query> - Get the latest news
-┃ ➸ ${prefix}define <word> - Get the definition of a word
-┃ ➸ ${prefix}translate <text> - Translate text to another language
+┃ 🎮 *FUN*
+┃ ┣ ${prefix}joke
+┃ ┣ ${prefix}meme
+┃ ┣ ${prefix}quote
+┃ ┣ ${prefix}flip
+┃ ┣ ${prefix}roll
+┃ ┣ ${prefix}8ball <q>
+┃ ┣ ${prefix}riddle
+┃ ┣ ${prefix}love <name1> <name2>
+┃ ┣ ${prefix}cat
+┃ ┣ ${prefix}dog
+┃ ┗ ${prefix}fact
 ┃
-┃ 📌 *Other Commands*:
-┃ ➸ ${prefix}help - Get a list of all commands
-┃ ➸ ${prefix}about - Get information about the bot
-┃ ➸ ${prefix}ping - Check the bot's response time
-┃ ➸ ${prefix}stats - Get bot statistics
-┃ ➸ ${prefix}invite - Get the bot's invite link
-┃ ➸ ${prefix}feedback <message> - Send feedback to the developers
+┃ ℹ️ *SYSTEM*
+┃ ┣ ${prefix}menu
+┃ ┣ ${prefix}ping
+┃ ┣ ${prefix}about
+┃ ┣ ${prefix}help
+┃ ┣ ${prefix}stats
+┃ ┣ ${prefix}invite
+┃ ┗ ${prefix}feedback <msg>
 ┃
-╰━━━━━━━━━━━━━━━━━━━━━━━━━━━━╯
-  `;
-
-    m.reply(menu);
-  } else {
-    m.reply(`Unknown command. Type ${prefix}menu to view the bot's menu.`);
-  }
-}
-
-module.exports = { menuCommand };
+╰━━━〔 POWERED BY DARK_TECH 〕━━━╯
+    `;
+    await sock.sendMessage(m.chat, { text: menuText }, { quoted: m });
+  },
+};
